eb22fcb347833571e2ed1f3701e3af37134cd1d2
Ekaterina <ekaterina.zink@gmail.com>
2018-09-25 23:05:04 -0300

    дз пункты 1,2 и частично 3

--- js/map.js
+++ js/map.js
@@ -1,34 +1,51 @@
 'use strict';
 
-var numberGuests = Math.floor(Math.random() * 100);
-var numberRooms = Math.floor(Math.random() * 5) + 1;
-var priceRandom = Math.floor(Math.random() * 999001) + 1000;
-var yLocation = Math.floor(Math.random() * 501) + 130;
-var userAvatar = Math.floor(Math.random() * 8) + 1;
+var random = Math.floor(Math.random());
+var numberGuests = random * 100;
+var numberRooms = random * 5 + 1;
+var priceRandom = random * 999001 + 1000;
+var yLocation = random * 501 + 130;
+var itemTitle = ["Большая уютная квартира", "Маленькая неуютная квартира", "Огромный прекрасный дворец", "Маленький ужасный дворец", "Красивый гостевой домик", "Некрасивый негостеприимный домик", "Уютное бунгало далеко от моря", "Неуютное бунгало по колено в воде"];
+var itemType = ["palace", "flat", "house", "bungalo"];
+var timeCheckin = ["12:00", "13:00", "14:00"];
+var timeCheckout = ["12:00", "13:00", "14:00"];
+var typeFeatures = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
+var xLocation = map.offsetWidth;
 
-var offerInformation = [
-{
+function getAvatar (){
+  return "0" + (random * 8 + 1);
+}
+
+var offerInformation = []
+
+function getBooking (array) {
   author = {
-    avatar: "user(userAvatar).png", //где {{xx}} это число от 1 до 8 с ведущим нулём. Например, 01, 02 и т. д. Адреса изображений не повторяются
+    avatar: "user(getAvatar).png", //где {{xx}} это число от 1 до 8 с ведущим нулём. Например, 01, 02 и т. д. Адреса изображений не повторяются
   },
 
   offer = {
-    title: ["Большая уютная квартира", "Маленькая неуютная квартира", "Огромный прекрасный дворец", "Маленький ужасный дворец", "Красивый гостевой домик", "Некрасивый негостеприимный домик", "Уютное бунгало далеко от моря", "Неуютное бунгало по колено в воде"],
-    address: "location.x, location.y",
+    title: itemTitle[random*itemTitle.length],
+    address: [location.x, location.y],
     price: priceRandom,
-    type: "palace, flat, house, bungalo",
+    type:  itemType[random*itemType.length],
     rooms: numberRooms,
     guests: numberGuests,
-    checkin: "12:00, 13:00, 14:00",
-    checkout: "12:00, 13:00, 14:00",
-    features : ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"],
+    checkin: timeCheckin[random*timeCheckin.length],
+    checkout: timeCheckout[random*timeCheckout .length],
+    features : typeFeatures.slice(random*7,random*8+1),
     description : '',
     photos : ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"]
   },
 
   location = {
-    x : случайное число, //координата x метки на карте. Значение ограничено размерами блока, в котором перетаскивается метка.
+    x : xLocation, //координата x метки на карте. Значение ограничено размерами блока, в котором перетаскивается метка.
     y : yLocation
   }
+
+  return array;
+};
+
+for (var i=0; i<10; i++){
+  offerInformation.push (array);
+
 }
-];
