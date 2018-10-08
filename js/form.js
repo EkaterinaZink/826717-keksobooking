60f1b9dffe7dd0f3bfe8e9ed5d5148ee89e42b58
Ekaterina <ekaterina.zink@gmail.com>
2018-10-07 22:00:02 -0300

    5.2 выполнено

--- js/form.js
+++ js/form.js
@@ -7,12 +7,7 @@
     'house': 5000,
     'palace': 10000,
   };
-  var countRoomsGuests = {
-    '1': ['1'],
-    '2': ['1', '2'],
-    '3': ['1', '2', '3'],
-    '100': ['0']
-  };
+
   var addRooms = document.querySelector('#room_number');
   var addCapacity = document.querySelector('#capacity');
   var addType = document.querySelector('#type');
@@ -22,15 +17,9 @@
 
   // установка соответствия количества гостей количеству комнат
   var getRooms = function () {
-    /*var rooms = addRooms.value;
-    var capacity = addCapacity.value;
-    var message = (countRoomsGuests[rooms].indexOf(capacity) === -1) ? 'Количество гостей не соответствует количеству комнат' : '';
-    */var message = (addRooms.value <= addCapacity.value) ? 'Количество гостей не соответствует количеству комнат' : '';
+    var message = (addRooms.value <= addCapacity.value) ? 'Количество гостей не соответствует количеству комнат' : '';
     if (addRooms.value === '100') {
       message = (addCapacity.value !== '0') ? 'Количество гостей не соответствует количеству комнат' : '';
-    }
-    if ((addRooms.value === null) && (addCapacity.value === null)) {
-      message = 'Количество гостей не соответствует количеству комнат';
     }
     addCapacity.setCustomValidity(message);
   };
